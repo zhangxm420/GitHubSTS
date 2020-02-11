191b334714cdd026586eff5e523e4d1ba4706f13
wangwuatcn <zxm98420@163.com>
2020-02-11 10:28:00 +0800

    test dev branch

--- src/com/atguigu/bean/TestGitHub.java
+++ src/com/atguigu/bean/TestGitHub.java
@@ -5,5 +5,6 @@ public class TestGitHub {
 	public static void main(String[] args) {
 		System.out.println("love from zhangxm420");
 		System.out.println("ok");
+		System.out.println("test dev branch");
 	}
 } 
